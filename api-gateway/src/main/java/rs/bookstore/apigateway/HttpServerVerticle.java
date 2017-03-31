98fe90cbc3cbcc8b22b1a8b41ba6c631d9c470cd
marko <marko@marko-Aspire-5560>
2017-03-31 23:02:26 +0200

    call customer-microservice from apigateway

--- api-gateway/src/main/java/rs/bookstore/apigateway/HttpServerVerticle.java
+++ api-gateway/src/main/java/rs/bookstore/apigateway/HttpServerVerticle.java
@@ -5,12 +5,15 @@
  */
 package rs.bookstore.apigateway;
 
-import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.mongo.HashSaltStyle;
 import io.vertx.ext.auth.mongo.MongoAuth;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CookieHandler;
 import io.vertx.ext.web.handler.FormLoginHandler;
@@ -19,17 +22,22 @@ import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.UserSessionHandler;
 import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.servicediscovery.types.EventBusService;
+import rs.bookstore.customer.service.Customer;
+import rs.bookstore.customer.service.CustomerService;
+import rs.bookstore.lib.MicroServiceVerticle;
 
 /**
  *
  * @author markom
  */
-public class HttpServerVerticle extends AbstractVerticle {
+public class HttpServerVerticle extends MicroServiceVerticle {
 
     MongoAuth authProvider;
 
     @Override
-    public void start() throws Exception {
+    public void start(Future<Void> startFuture) throws Exception {
+        super.start();
         String uri = "mongodb://localhost:27017";
         String db = "master";
 
@@ -40,15 +48,13 @@ public class HttpServerVerticle extends AbstractVerticle {
         MongoClient client = MongoClient.createShared(vertx, mongoconfig);
         JsonObject authProperties = new JsonObject();
         authProvider = MongoAuth.create(client, authProperties);
-        authProvider.setCollectionName("account");
+        authProvider.setCollectionName("user");
         authProvider.setPasswordField("password");
         authProvider.setUsernameField("username");
         authProvider.getHashStrategy().setSaltStyle(HashSaltStyle.NO_SALT);
         authProvider.setPermissionField("permission");
 
         Router router = Router.router(vertx);
-
-        vertx.createHttpServer().requestHandler(router::accept).listen(8500, "localhost");
 
         // We need cookies, sessions and request bodies
         router.route().handler(CookieHandler.create());
@@ -66,6 +72,7 @@ public class HttpServerVerticle extends AbstractVerticle {
 
         // Handles the actual login
         router.post("/loginhandler").handler(FormLoginHandler.create(authProvider));
+        router.route("/customer/get").handler(this::getCustomerCredentials);
 
         // Implement logout
         router.route("/logout").handler(context -> {
@@ -77,28 +84,66 @@ public class HttpServerVerticle extends AbstractVerticle {
         // Serve the non private static pages
         router.route().handler(StaticHandler.create());
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8500);
+        vertx.createHttpServer().requestHandler(router::accept).listen(8500, ar -> {
+            if (ar.succeeded()) {
+                startFuture.complete();
+            } else {
+                startFuture.fail(ar.cause());
+            }
+        });
+    }
+
+    private void getCustomerCredentials(RoutingContext rc) {
+        if (rc.user() == null) {
+            rc.response().setStatusCode(401).end("No user authenticated");
+            return;
+        }
+
+        JsonObject principal = rc.user().principal();
+        System.out.println("Principal is: " + principal);
+        String username = principal.getString("username");
+        Long customer_id = principal.getLong("customer_id");
+
+        if (username == null || customer_id == null) {
+            rc.response()
+                    .putHeader("content-type", "application/json")
+                    .end(new Customer(
+                            new JsonObject()
+                            .put("username", "testUsername")
+                            .put("id", "testId")).toJson().encodePrettily());
+        } else {
+            Future<CustomerService> futureCustomerService = Future.future();
+            EventBusService.getServiceProxyWithJsonFilter(
+                    discovery,
+                    new JsonObject().put("name", CustomerService.SERVICE_NAME),
+                    CustomerService.class,
+                    futureCustomerService.completer());
+            futureCustomerService.compose(customerService -> {
+                Future<Customer> customerFuture = Future.future();
+                customerService.getCustomerByUsername(username, customerFuture.completer());
+                return customerFuture;
+            })
+                    .setHandler(resultHandlerNonEmpty(rc));
+        }
 
     }
 
-//    private void logIn(RoutingContext rc) {
-//        String username = rc.request().getParam("username");
-//        String password = rc.request().getParam("password");
-//
-//        System.out.println("PARAMS: " + rc.request().params());
-//        JsonObject authInfo = new JsonObject()
-//                .put("username", username)
-//                .put("password", password);
-//        authProvider.authenticate(authInfo, res -> {
-//            if (res.succeeded()) {
-//                User user = res.result();
-//                user.setAuthProvider(authProvider);
-//                rc.setUser(user);
-//                rc.reroute("/private/private_page");
-//            } else {
-//                // Failed!
-//                res.cause().printStackTrace();
-//            }
-//        });
-//    }
+    //TO DO: refactor
+    private <T> Handler<AsyncResult<T>> resultHandlerNonEmpty(RoutingContext context) {
+        return ar -> {
+            if (ar.succeeded()) {
+                T res = ar.result();
+                if (res == null) {
+                    context.response().end("NOT found....see line 137");
+                } else {
+                    context.response()
+                            .putHeader("content-type", "application/json")
+                            .end(res.toString());
+                }
+            } else {
+                context.response().end(ar.cause().getMessage());
+                ar.cause().printStackTrace();
+            }
+        };
+    }
 }
